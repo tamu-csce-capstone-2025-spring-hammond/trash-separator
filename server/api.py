a88ef249ebcab079d8c077090f0b82c956965f44
qurs3l <qureshiseline@gmail.com>
2025-04-03 11:17:49 -0500

    added files for backend deployment

--- server/api.py
+++ server/api.py
@@ -62,4 +62,4 @@ def predict():
         return jsonify({"error": f"Failed to process image: {e}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
